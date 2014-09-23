34ec826d51a30da0d5603463c67787fe5c30144b
Christopher Pappas <cpappas@avvo.com>
2014-09-23 10:21:48 -0700

    updated tests

--- test/minitext/default_settings_test.rb
+++ test/minitext/default_settings_test.rb
@@ -7,22 +7,18 @@ module Minitext
       Minitext::Base.gateway = Minitext::TestGateway.new
     end
 
-    def test_doesthiswork
-      Minitext.set_defaults('hey')
-    end
-
     def test_default_settings_set
-      Minitext.set_defaults(from: '1234567890', to: '2222222222', body: 'default message')
+      Minitext.set_defaults(from: '1234567890', to: '5558675309', body: 'This is a test text.')
       assert_equal Message.defaults[:from], '1234567890'
-      assert_equal Message.defaults[:to], '2222222222'
-      assert_equal Message.defaults[:body], 'default message'
+      assert_equal Message.defaults[:to], '5558675309'
+      assert_equal Message.defaults[:body], 'This is a test text.'
     end
 
     def test_single_defaults
       Minitext.set_defaults(from: '1234567890')
       assert Message.new(to: '5558675309', body: 'This is a test text.').valid?, 'Default field not found'
 
-      Minitext.set_defaults(to: '2222222222')
+      Minitext.set_defaults(to: '5558675309')
       assert Message.new(from: '1234567890', body: 'This is a test text.').valid?, 'Default field not found'
 
       Minitext.set_defaults(body: 'default message')
